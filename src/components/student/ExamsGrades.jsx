b32d3aad18b41d1c7a63adf273cf7d69ef5e9f0c
segundara <segundara@gmail.com>
2020-10-08 05:54:11 +0300

    added pdf download for academic transcript

--- src/components/student/ExamsGrades.jsx
+++ src/components/student/ExamsGrades.jsx
@@ -1,12 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Alert, Button } from 'react-bootstrap';
 import authAxios from "../../lib/http"
 import Cookies from "js-cookie"
 import axios from "axios"
+import download from 'downloadjs'
 
 const ExamsGrades = ({ userID, updateData }) => {
     const [data, setData] = useState(null)
+    // const [pdfFile, setPdfFile] = useState('')
+    const [success, setSuccess] = useState(false)
+    const [failure, setFailure] = useState(false)
     const [loading, setLoading] = useState(true)
+
+    const getPDF = async () => {
+        try {
+            const res = await authAxios.get(`/exams/${userID}/pdf`, { responseType: 'blob', withCredentials: true })
+            let result = []
+
+            if (!res) {
+                const secondRes = await axios.get(`${process.env.REACT_APP_API_URL}/exams/${userID}/pdf`, {
+                    headers: { Authorization: `Bearer ${Cookies.get("accessToken")}` },
+                    responseType: 'blob',
+                    withCredentials: true,
+                })
+                result = await secondRes
+            } else {
+                result = await res
+            }
+            const content = result.headers['content-type'];
+            download(result.data, 'Transcript', content)
+
+            setSuccess(true)
+            setTimeout(() => {
+                setSuccess(false)
+            }, 5000);
+
+        } catch (error) {
+            console.log(error)
+            setFailure(true)
+            setTimeout(() => {
+                setFailure(false)
+            }, 5000);
+        }
+
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -40,6 +77,16 @@ const ExamsGrades = ({ userID, updateData }) => {
     return (
         <>
             <div>
+                <Alert variant="info" show={success} >
+                    <strong>Record downloaded</strong>
+                </Alert>
+                <Alert variant="danger" show={failure} >
+                    <strong>
+                        Something went wrong!!!
+                </strong>
+                </Alert>
+            </div>
+            <div>
 
                 <Table striped bordered hover>
                     <thead>
@@ -72,6 +119,7 @@ const ExamsGrades = ({ userID, updateData }) => {
                         }
                     </tbody>
                 </Table>
+                <Button onClick={getPDF}>Download Transcript</Button>{' '}
             </div>
         </>
     )
