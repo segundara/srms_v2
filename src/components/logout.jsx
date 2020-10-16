6c3a3e01b312b6a5a28957c601906d431d925f99
segundara <segundara@gmail.com>
2020-10-16 11:58:31 +0300

    more

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -29,7 +29,7 @@ const Logout = (props) => {
                     // console.log(response)
                     props.status(!isLoggedin)
                     props.userTitle(userTitle)
-                    props.history.push('/login');
+                    props.history.push('/');
                 }
 
             } catch (error) {
