9150cd52621d6f022630b0d820985f1e015e9c23
segundara <segundara@gmail.com>
2020-10-21 03:04:36 +0300

    revert changes to structure

--- src/components/tutor/Tutor.jsx
+++ src/components/tutor/Tutor.jsx
@@ -8,141 +8,131 @@ import StudentList from './MyStudentList'
 import ExamsGrades from '../tutor/ExamsGrades';
 import "../allrouteStyle/style.scss";
 
-const TutorDetail = ({ userTitle, currentUser }) => {
-    // const [currentUser, setCurrentUser] = useState('');
-    // const [studentsRecord, setStudentsRecord] = useState('');
-    // const [loading, setLoading] = useState(true);
+const TutorDetail = ({ userTitle }) => {
+    const [currentUser, setCurrentUser] = useState('');
+    const [studentsRecord, setStudentsRecord] = useState('');
+    const [loading, setLoading] = useState(true);
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const res = await authAxios.get(`/${userTitle}/me`, { withCredentials: true })
-    //             // const res = await axios.get(`/${userTitle}/me`,
-    //             //     {
-    //             //         baseURL: process.env.REACT_APP_API_URL,
-    //             //         headers: { Authorization: `Bearer ${localStorage.getItem("accessToken")}` },
-    //             //     }
-    //             // )
-    //             let currentUser = []
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await authAxios.get(`/${userTitle}/me`, { withCredentials: true })
+                // const res = await axios.get(`/${userTitle}/me`,
+                //     {
+                //         baseURL: process.env.REACT_APP_API_URL,
+                //         headers: { Authorization: `Bearer ${localStorage.getItem("accessToken")}` },
+                //     }
+                // )
+                let currentUser = []
 
-    //             if (!res) {
-    //                 const secondRes = await axios.get(`${process.env.REACT_APP_API_URL}/${userTitle}/me`, {
-    //                     headers: { Authorization: `Bearer ${Cookies.get("accessToken")}` },
-    //                     withCredentials: true,
-    //                 })
-    //                 currentUser = secondRes.data
-    //             } else {
-    //                 currentUser = res.data
-    //             }
+                if (!res) {
+                    const secondRes = await axios.get(`${process.env.REACT_APP_API_URL}/${userTitle}/me`, {
+                        headers: { Authorization: `Bearer ${Cookies.get("accessToken")}` },
+                        withCredentials: true,
+                    })
+                    currentUser = secondRes.data
+                } else {
+                    currentUser = res.data
+                }
 
-    //             setCurrentUser(currentUser)
-    //             setLoading(false)
+                setCurrentUser(currentUser)
+                setLoading(false)
 
-    //         } catch (error) {
-    //             console.log(error)
-    //         }
+            } catch (error) {
+                console.log(error)
+            }
 
-    //     }
-    //     fetchData();
-    // }, []);
+        }
+        fetchData();
+    }, []);
 
-    // const updateUserinfo = (newInfo) => { setCurrentUser(newInfo) }
-    // const getRecords = (newData) => { setStudentsRecord(newData) }
+    const updateUserinfo = (newInfo) => { setCurrentUser(newInfo) }
+    const getRecords = (newData) => { setStudentsRecord(newData) }
 
     return (
         <>
-            {/* {loading ? <div>...loading</div>
-                : ( */}
-            <>
-                <Row>
-                    <Col>
-                        <Row className="mt-4 mb-2">
-                            <Col md={3}>
-                                {currentUser && currentUser.image
-                                    ? <Image src={currentUser.image} className="img-fluid rounded-circle" alt="profile" />
-                                    : <Image src='https://img.icons8.com/officel/2x/user.png' className="img-fluid rounded-circle" alt="profile" />
-                                }
-                            </Col>
-                            <Col md={9} className="d-flex flex-column justify-content-center">
-                                <h3>{currentUser.firstname} {currentUser.lastname}</h3>
-                                <strong>{currentUser.email}</strong>
+            {loading ? <div>...loading</div>
+                : (
+                    <>
+                        <Row>
+                            <Col>
+                                <Row className="mt-4 mb-2">
+                                    <Col md={3}>
+                                        {currentUser && currentUser.image
+                                            ? <Image src={currentUser.image} className="img-fluid rounded-circle" alt="profile" />
+                                            : <Image src='https://img.icons8.com/officel/2x/user.png' className="img-fluid rounded-circle" alt="profile" />
+                                        }
+                                    </Col>
+                                    <Col md={9} className="d-flex flex-column justify-content-center">
+                                        <h3>{currentUser.firstname} {currentUser.lastname}</h3>
+                                        <strong>{currentUser.email}</strong>
+                                    </Col>
+                                </Row>
                             </Col>
                         </Row>
-                    </Col>
-                </Row>
-                <Accordion defaultActiveKey="1">
-                    <Card className="card mb-4">
-                        <Card.Header className="card-header">
-                            <Accordion.Toggle as={Button} eventKey="0" className="accordion-nav">
-                                My Account
+                        <Accordion defaultActiveKey="1">
+                            <Card className="card mb-4">
+                                <Card.Header className="card-header">
+                                    <Accordion.Toggle as={Button} eventKey="0" className="accordion-nav">
+                                        My Account
                                     </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey="0">
-                            <Card.Body>
-                                <>
-                                    <Table responsive="sm">
-                                        <thead>
-                                            <tr>
-                                                <th className="text-left" colSpan="2">Profile Detail</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>First Name</td>
-                                                <td><strong>{currentUser.firstname}</strong></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Last Name</td>
-                                                <td><strong>{currentUser.lastname}</strong></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Email</td>
-                                                <td><strong>{currentUser.email}</strong></td>
-                                            </tr>
-                                        </tbody>
-                                    </Table>
-                                    <TutorProfile
-                                        currentUser={currentUser}
-                                        // updateUser={updateUserinfo}
-                                        userTitle={userTitle}
-                                    />
-                                </>
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                    <Card className="card mb-4">
-                        <Card.Header className="card-header">
-                            <Accordion.Toggle as={Button} eventKey="1" className="accordion-nav">
-                                My Student List
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                    <Card.Body>
+                                        <>
+                                            <Table responsive="sm">
+                                                <thead>
+                                                    <tr>
+                                                        <th className="text-left" colSpan="2">Profile Detail</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>First Name</td>
+                                                        <td><strong>{currentUser.firstname}</strong></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Last Name</td>
+                                                        <td><strong>{currentUser.lastname}</strong></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Email</td>
+                                                        <td><strong>{currentUser.email}</strong></td>
+                                                    </tr>
+                                                </tbody>
+                                            </Table>
+                                            <TutorProfile
+                                                currentUser={currentUser}
+                                                updateUser={updateUserinfo}
+                                                userTitle={userTitle}
+                                            />
+                                        </>
+                                    </Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                            <Card className="card mb-4">
+                                <Card.Header className="card-header">
+                                    <Accordion.Toggle as={Button} eventKey="1" className="accordion-nav">
+                                        My Student List
                                     </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey="1">
-                            <Card.Body>
-                                <StudentList
-                                    userID={currentUser._id}
-                                // studentsRecord={getRecords} 
-                                />
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                    <Card className="card mb-4">
-                        <Card.Header className="card-header">
-                            <Accordion.Toggle as={Button} eventKey="2" className="accordion-nav">
-                                Exams/Grades
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="1">
+                                    <Card.Body><StudentList userID={currentUser._id} studentsRecord={getRecords} /></Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                            <Card className="card mb-4">
+                                <Card.Header className="card-header">
+                                    <Accordion.Toggle as={Button} eventKey="2" className="accordion-nav">
+                                        Exams/Grades
                                     </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey="2">
-                            <Card.Body>
-                                <ExamsGrades
-                                    userID={currentUser._id}
-                                // studentsRecord={studentsRecord} 
-                                />
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                </Accordion>
-            </>
-            {/* )} */}
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="2">
+                                    <Card.Body><ExamsGrades userID={currentUser._id} studentsRecord={studentsRecord} /></Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
+                    </>
+                )}
         </>
     )
 }
