bf1f6193141448656ea65fbc04c6018eb6d1e9fa
segundara <segundara@gmail.com>
2020-10-24 14:35:26 +0300

    edit

--- src/components/student/Student.jsx
+++ src/components/student/Student.jsx
@@ -74,7 +74,7 @@ const StudentDetail = ({ userTitle, currentUser, updateUserInfo, updateData, use
                         <Col md={9} className="d-flex flex-column justify-content-center">
                             <Accordion defaultActiveKey="1">
                                 <Card className="card mb-4">
-                                    <CustomToggle as={Card.Header} eventKey="0" className="accordion-nav">
+                                    <CustomToggle as={Card.Header} eventKey="0" style={{ backgroundColor: '#6c63ff !important', cursor: 'pointer' }}>
                                         My Account
                                     </CustomToggle>
                                     {/* <Accordion.Toggle as={Card.Header} eventKey="0" className="accordion-nav">
