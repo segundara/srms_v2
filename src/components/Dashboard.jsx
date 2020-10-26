9073cbbf4f40d3e1244685aaa7451ca44d2a9d11
segundara <segundara@gmail.com>
2020-10-26 17:44:39 +0200

    dashboard title

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,31 +1,33 @@
-import React from "react"
-import { Container } from "react-bootstrap"
-import StudentDetail from './student/Student'
-import TutorDetail from './tutor/Tutor'
-import AdminDetail from './admin/Admin'
-import JumBotron from "./welcome/Welcome"
-import "./allrouteStyle/style.scss"
-import UserData from "./UserPage"
+import React from "react";
+import { Container } from "react-bootstrap";
+import StudentDetail from "./student/Student";
+import TutorDetail from "./tutor/Tutor";
+import AdminDetail from "./admin/Admin";
+import JumBotron from "./welcome/Welcome";
+import "./allrouteStyle/style.scss";
+import UserData from "./UserPage";
 
 const Dashboard = (props) => {
+  console.log("userTitle => ", props.userTitle);
+  console.log("userTitle => ", props);
 
-    console.log("userTitle => ", props.userTitle)
-    console.log("userTitle => ", props)
-
-    return (
+  return (
+    <>
+      {props.userTitle.length > 0 ? (
         <>
-            {props.userTitle.length > 0
-                ? (
-                    <>
-                        <Container className="mt-3">
-                            <h1>{props.userTitle.toUpperCase()} DASHBOARD</h1>
-                            <UserData userTitle={props.userTitle} />
-                        </Container>
-                    </>
-                )
-                : <JumBotron />
-            }
+          <Container className="mt-3">
+            <h1>
+              {props.userTitle.charAt(0).toUpperCase() +
+                props.userTitle.slice(1)}{" "}
+              Dashboard
+            </h1>
+            <UserData userTitle={props.userTitle} />
+          </Container>
         </>
-    )
-}
-export default Dashboard
+      ) : (
+        <JumBotron />
+      )}
+    </>
+  );
+};
+export default Dashboard;
